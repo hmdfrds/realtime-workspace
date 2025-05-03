f26e9fc3c5ba56449de4efc4f91d1d4b9188473c
Firdaus <firdaus1337@gmail.com>
2025-05-04 03:54:27 +0800

    authentication middleware

---
+++ server/src/middleware/authMiddleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../lib/jwt";
+
+export function authenticateToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader?.split(" ")[1];
+
+  if (!token) {
+    res.status(401).json({ message: "Access denied. No token provided." });
+    return;
+  }
+
+  try {
+    const payload = verifyToken(token);
+
+    req.user = { id: payload.userId };
+
+    next();
+  } catch (error) {
+    console.error("Authentication Error:", error);
+    return res
+      .status(403)
+      .json({ message: "Access denied. Invalid or expired token" });
+  }
+}
